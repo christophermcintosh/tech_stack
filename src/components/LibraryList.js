fcab94b35ad019e012922f10c345f8db1683f90a
Christopher McIntosh <chrismac92@gmail.com>
2018-07-09 18:47:43 -0400

    added redux

---
+++ src/components/LibraryList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import { connect } from 'react-redux';
+import ListItem from './ListItem';
+
+class LibraryList extends Component {
+  renderItem(library) {}
+
+  render() {
+    console.log(this.props.libraries);
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={library => library.id}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
